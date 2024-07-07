fae083901c1ada80538840296334a158cf6cc070
Michael Wiltfong <76107997+mdwiltfong@users.noreply.github.com>
2024-07-06 22:54:19 -0400

    chore: Refactor API configuration and metrics handling

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
@@ -10,8 +11,13 @@ func main() {
 	const port = "8080"
 
 	mux := http.NewServeMux()
-	mux.Handle("/app/*", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
-	mux.HandleFunc("/healthz", handlerReadiness)
+	apiCfg := apiConfig{}
+	mux.Handle("/app/*", http.StripPrefix("/app",
+		apiCfg.middlewareMetricInc(http.FileServer(http.Dir(filepathRoot)))))
+
+	mux.HandleFunc("GET /api/healthz", handlerReadiness)
+	mux.HandleFunc("GET /api/metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("/api/reset", apiCfg.handleReset)
 
 	srv := &http.Server{
 		Addr:    ":" + port,
@@ -20,6 +26,31 @@ func main() {
 
 	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(srv.ListenAndServe())
+}
+
+type apiConfig struct {
+	filserverHits int
+}
+
+func (cgf *apiConfig) middlewareMetricInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cgf.filserverHits++
+		next.ServeHTTP(w, r)
+	})
+}
+func (cgf *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	body := fmt.Sprintf("Hits: %v", cgf.filserverHits)
+	w.Write([]byte(body))
+}
+
+func (cgf *apiConfig) handleReset(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	cgf.filserverHits = 0
+	body := fmt.Sprintf("Hits: %v", cgf.filserverHits)
+	w.Write([]byte(body))
 }
 
 func handlerReadiness(w http.ResponseWriter, r *http.Request) {
